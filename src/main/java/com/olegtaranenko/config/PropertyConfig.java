2dc6ed61a417e2fb420d2449d3327e9385ef09be
Oleg Taranenko MP <olegtaranenko@gmail.com>
2019-03-03 16:14:06 +0100

    application properties only

--- src/main/java/com/olegtaranenko/config/PropertyConfig.java
+++ src/main/java/com/olegtaranenko/config/PropertyConfig.java
@@ -2,26 +2,12 @@ package com.olegtaranenko.config;
 
 import com.olegtaranenko.examplebeans.FakeDataSource;
 import com.olegtaranenko.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties"),
-
-})
 public class PropertyConfig {
-
-    @Autowired
-    Environment env;
-
     @Value("${guru.username}")
     String user;
 
@@ -44,7 +30,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakedDataSource = new FakeDataSource();
-        fakedDataSource.setUser(env.getProperty("GURU_USERNAME"));
+        fakedDataSource.setUser(user);
         fakedDataSource.setPassword(password);
         fakedDataSource.setUrl(url);
 
@@ -59,10 +45,5 @@ public class PropertyConfig {
         fakedJmsBroker.setUrl(jmsUrl);
 
         return fakedJmsBroker;
-    }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer prorperties() {
-        return new PropertySourcesPlaceholderConfigurer();
     }
 }
